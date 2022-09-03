bdd6227f6ba4d8bf20309358a0a1a419556d6da9
Simpledaxt <vladknelmi@gmail.com>
2022-09-03 17:05:02 +0300

    initial

--- my-js-file.js
+++ my-js-file.js
@@ -7,15 +7,15 @@ info.textContent = 'Below is a dynamic list. Click anywhere on the page to add a
 document.body.appendChild(info);
 document.body.appendChild(list);
 
-html.onclick = function() {
-  const listItem = document.createElement('li');
-  const listContent = prompt('What content do you want the list item to have?');
-  listItem.textContent = listContent;
-  list.appendChild(listItem);
+//html.on = function() {
+ // const listItem = document.createElement('li');
+  //const listContent = prompt('What content do you want the list item to have?');
+ // listItem.textContent = listContent;
+ // list.appendChild(listItem);
 
-  listItem.onclick = function(e) {
-    e.stopPropagation();
-    const listContent = prompt('Enter new content for your list item');
-    this.textContent = listContent;
-  }
-}
+ // listItem.onclick = function(e) {
+  //  e.stopPropagation();
+  //  const listContent = prompt('Enter new content for your list item');
+  //  this.textContent = listContent;
+ // }
+//}
